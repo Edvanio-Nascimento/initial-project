20baa80acc6817c6a0b1b75f4dfd8cd1f1b43e1d
Edy Nascimento <nascoliedy@gmail.com>
2025-07-18 13:17:27 -0400

    Iniciando o porjeto

---
+++ src/main/java/com/edy/course/intialproject/IntialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edy.course.intialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edy/course/intialproject/IntialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edy.course.intialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
