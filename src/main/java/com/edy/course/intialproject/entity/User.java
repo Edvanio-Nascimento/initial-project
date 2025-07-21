5409d48e5190a7e2a8f6b44c832c9219ea4f8aa0
Edy Nascimento <nascoliedy@gmail.com>
2025-07-21 09:33:53 -0400

    creating entity user

--- src/main/java/com/edy/course/intialproject/entity/User.java
+++ src/main/java/com/edy/course/intialproject/entity/User.java
@@ -1,4 +1,87 @@
 package com.edy.course.intialproject.entity;
 
-public class User {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity @Table(name = "tb_users")
+public class User implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 70)
+    private String name;
+
+    @Column(name = "login", nullable = false, unique = true, length = 20)
+    private String login;
+
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 30)
+    private String email;
+
+    public User() {
+    }
+
+    public User(UUID id, String name, String login, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
